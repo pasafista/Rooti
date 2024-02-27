60b48312811c403e704ba3b66c6792cfcaae7aa0
Toji <143400092+pasafista@users.noreply.github.com>
2024-02-27 13:32:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+<script>
+    var countDownDate = new Date();
+    countDownDate.setDate(countDownDate.getDate() + 3);
+
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerHTML = formatTime(days);
+        document.getElementById("hours").innerHTML = formatTime(hours);
+        document.getElementById("minutes").innerHTML = formatTime(minutes);
+        document.getElementById("seconds").innerHTML = formatTime(seconds);
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+
+    function formatTime(time) {
+        return time < 10 ? "0" + time : time;
+    }
+</script>
 
